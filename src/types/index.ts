6ad1d227ffb7b68b414b70f9375122f9a9b92eee
goShell3 <herkodandy@gmail.com>
2026-04-05 12:03:26 +0300

    Feat: update the messages and settings to remove the role based auth

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,36 +2,36 @@ import type { components, operations } from './api';
 
 // Auth
 export type Token = components['schemas']['Token'];
-// export type UserLogin = components['schemas']['UserLogin'];
-// export type UserCreate = components['schemas']['UserCreate'];
-// export type UserResponse = components['schemas']['UserResponse'];
+export type UserLogin = components['schemas']['UserLogin'];
+export type UserCreate = components['schemas']['UserCreate'];
+export type UserResponse = components['schemas']['UserResponse'];
 
 // Leads
-// export type Lead = components['schemas']['LeadResponse'];
-// export type LeadCreate = components['schemas']['LeadCreate'];
-// export type LeadUpdate = components['schemas']['LeadUpdate'];
-// export type LeadListResponse = components['schemas']['LeadListResponse'];
-// export type LeadScoreResponse = components['schemas']['LeadScoreResponse'];
-// export type LeadSegmentUpdate = components['schemas']['LeadSegmentUpdate'];
-// export type LeadSegment = components['schemas']['LeadSegment'];
-// export type LeadSource = components['schemas']['LeadSource'];
-// export type ConsentStatus = components['schemas']['ConsentStatus'];
+export type Lead = components['schemas']['LeadResponse'];
+export type LeadCreate = components['schemas']['LeadCreate'];
+export type LeadUpdate = components['schemas']['LeadUpdate'];
+export type LeadListResponse = components['schemas']['LeadListResponse'];
+export type LeadScoreResponse = components['schemas']['LeadScoreResponse'];
+export type LeadSegmentUpdate = components['schemas']['LeadSegmentUpdate'];
+export type LeadSegment = components['schemas']['LeadSegment'];
+export type LeadSource = components['schemas']['LeadSource'];
+export type ConsentStatus = components['schemas']['ConsentStatus'];
 
 // Campaigns
-// export type Campaign = components['schemas']['CampaignResponse'];
-// export type CampaignCreate = components['schemas']['CampaignCreate'];
-// export type CampaignUpdate = components['schemas']['CampaignUpdate'];
-// export type CampaignListResponse = components['schemas']['CampaignListResponse'];
-// export type CampaignStats = components['schemas']['CampaignStatsResponse'];
-// export type CampaignStatus = components['schemas']['CampaignStatus'];
-// export type CampaignType = components['schemas']['CampaignType'];
-// export type TriggerEvent = components['schemas']['TriggerEvent'];
+export type Campaign = components['schemas']['CampaignResponse'];
+export type CampaignCreate = components['schemas']['CampaignCreate'];
+export type CampaignUpdate = components['schemas']['CampaignUpdate'];
+export type CampaignListResponse = components['schemas']['CampaignListResponse'];
+export type CampaignStats = components['schemas']['CampaignStatsResponse'];
+export type CampaignStatus = components['schemas']['CampaignStatus'];
+export type CampaignType = components['schemas']['CampaignType'];
+export type TriggerEvent = components['schemas']['TriggerEvent'];
 // export type MessageChannel = components['schemas']['app__models__campaign__MessageChannel'];
 // export type AIMessageChannel = components['schemas']['app__schemas__ai__MessageChannel'];
 
 // Templates
-// export type MessageTemplate = components['schemas']['MessageTemplateResponse'];
-// export type MessageTemplateCreate = components['schemas']['MessageTemplateCreate'];
+export type MessageTemplate = components['schemas']['MessageTemplateResponse'];
+export type MessageTemplateCreate = components['schemas']['MessageTemplateCreate'];
 
 // AI
 // export type AIUsageStats = components['schemas']['AIUsageStats'];
@@ -57,10 +57,10 @@ export type Token = components['schemas']['Token'];
 // export type VerifyOTPRequest = components['schemas']['VerifyOTPRequest'];
 // export type OTPVerifyResponse = components['schemas']['OTPVerifyResponse'];
 
-// // Query params
-// export type ListLeadsParams = operations['list_leads_api_v1_leads_get']['parameters']['query'];
-// export type ListCampaignsParams = operations['list_campaigns_api_v1_campaigns_get']['parameters']['query'];
-// export type ListTemplatesParams = operations['list_templates_api_v1_templates_get']['parameters']['query'];
+// Query params
+export type ListLeadsParams = operations['list_leads_api_v1_leads_get']['parameters']['query'];
+export type ListCampaignsParams = operations['list_campaigns_api_v1_campaigns_get']['parameters']['query'];
+export type ListTemplatesParams = operations['list_templates_api_v1_templates_get']['parameters']['query'];
 
 // Calendar
 export type CalendarEventType = 'campaign' | 'meeting' | 'sms' | 'reminder';
